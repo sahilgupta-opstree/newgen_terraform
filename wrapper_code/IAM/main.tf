5864611b6903ec6e8d70be50c78463eda86970fd
sahilgupta-opstree <sahil.gupta@opstree.com>
2026-02-16 15:21:43 +0530

    Update main.tf

--- wrapper_code/IAM/main.tf
+++ wrapper_code/IAM/main.tf
@@ -1,5 +1,5 @@
 module "iam_role" {
-  source = "/home/sahilgupta/OT-CLOUD-TF-MODULE/terraform-aws-iam-role"
+  source = "git::https://github.com/sahilgupta-opstree/newgen_terraform.git//modules/terraform-aws-iam-role?ref=feature"
 
   region               = var.region
   roles                = var.roles
