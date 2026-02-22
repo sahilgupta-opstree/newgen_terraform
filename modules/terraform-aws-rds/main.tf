368cc1a5ce60aaef0744a9aabff31cc6d6243131
sahilgupta-opstree <sahil.gupta@opstree.com>
2026-02-22 15:57:59 +0530

    updating the RDS module files

--- modules/terraform-aws-rds/main.tf
+++ modules/terraform-aws-rds/main.tf
@@ -1,4 +1,4 @@
-resource "aws_rds_cluster" "this" {
+resource "aws_rds_cluster" "rds" {
   cluster_identifier     = var.cluster_identifier
   engine                 = var.engine
   engine_version         = var.engine_version
@@ -6,19 +6,21 @@ resource "aws_rds_cluster" "this" {
   master_password        = var.master_password
   port                   = var.port
   db_subnet_group_name   = var.db_subnet_group_name
-  vpc_security_group_ids = var.vpc_security_group_ids  # <-- use SG from root module
+  vpc_security_group_ids = var.vpc_security_group_ids  
   storage_encrypted      = var.storage_encrypted
   deletion_protection    = var.deletion_protection
   skip_final_snapshot    = var.skip_final_snapshot
+  tags                   = var.tags
 }
 
 
-resource "aws_rds_cluster_instance" "this" {
+resource "aws_rds_cluster_instance" "rds_instance" {
   count               = var.cluster_instance_count
   identifier          = "${var.cluster_identifier}-${count.index + 1}"
-  cluster_identifier  = aws_rds_cluster.this.id
+  cluster_identifier  = aws_rds_cluster.rds.id
   instance_class      = var.instance_class
   engine              = var.engine
   engine_version      = var.engine_version
   publicly_accessible = var.publicly_accessible
+  tags                = var.tags
 }
--- modules/terraform-aws-rds/output.tf
+++ modules/terraform-aws-rds/output.tf
@@ -1,35 +1,35 @@
 # Cluster endpoint for application connection
 output "cluster_endpoint" {
-  value       = aws_rds_cluster.this.endpoint
+  value       = aws_rds_cluster.rds.endpoint
   description = "The endpoint of the RDS cluster"
 }
 
 # Reader endpoint for read replicas
 output "reader_endpoint" {
-  value       = aws_rds_cluster.this.reader_endpoint
+  value       = aws_rds_cluster.rds.reader_endpoint
   description = "The reader endpoint for the RDS cluster"
 }
 
 # Cluster ID
 output "cluster_id" {
-  value       = aws_rds_cluster.this.id
+  value       = aws_rds_cluster.rds.id
   description = "The ID of the RDS cluster"
 }
 
 # Cluster ARN
 output "cluster_arn" {
-  value       = aws_rds_cluster.this.arn
+  value       = aws_rds_cluster.rds.arn
   description = "The ARN of the RDS cluster"
 }
 
 # Cluster port
 output "cluster_port" {
-  value       = aws_rds_cluster.this.port
+  value       = aws_rds_cluster.rds.port
   description = "The port on which the RDS cluster is listening"
 }
 
 # Instance IDs
 output "instance_ids" {
-  value       = aws_rds_cluster_instance.this[*].id
+  value       = aws_rds_cluster_instance.rds_instance[*].id
   description = "IDs of all RDS cluster instances"
 }
