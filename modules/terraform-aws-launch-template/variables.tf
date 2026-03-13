6fd879b0165cbfd628181b07310fa0fd80f9218f
sahilgupta-opstree <sahil.gupta@opstree.com>
2026-03-13 18:51:26 +0530

    updated main.tf and variable.tf

--- modules/terraform-aws-launch-template/variables.tf
+++ modules/terraform-aws-launch-template/variables.tf
@@ -7,11 +7,6 @@ variable "name_prefix" {
   description = "Launch template name prefix"
 }
 
-variable "key_name" {
-  type        = string
-  description = "Key pair name for EC2"
-}
-
 variable "image_id" {
   type        = string
   description = "AMI ID for the EC2 instance"
