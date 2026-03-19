b9b6169fe85fcc2d839d553355fa875905ac9a1a
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-03-19 11:17:52 +0530

    Update main.tf

--- modules/terraform-aws-db-subnet-group/main.tf
+++ modules/terraform-aws-db-subnet-group/main.tf
@@ -2,5 +2,10 @@ resource "aws_db_subnet_group" "this" {
   name       = var.name
   subnet_ids = var.subnet_ids
 
-  tags = var.tags
+  tags = merge(
+    {
+      Name = "${local.base_name}"
+    },
+    local.db_subnet_group_tags
+  )
 }
