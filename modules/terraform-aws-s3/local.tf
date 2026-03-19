f536cf08012cf030efebe6880dd4986e83171eff
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-03-19 11:32:38 +0530

    Update local.tf

--- modules/terraform-aws-s3/local.tf
+++ modules/terraform-aws-s3/local.tf
@@ -1,17 +1,13 @@
 locals {
-  base_name = "${var.env}-${var.app}"
+  create_bucket     = var.create_bucket
+  create_bucket_acl = var.acl != null && var.acl != "null"
+  attach_policy     = var.attach_elb_log_delivery_policy || var.attach_lb_log_delivery_policy || var.attach_iam_policy || var.attach_cloudtrail_policy
+  cors_rules        = var.cors_rules
 
-  common_tags = {
-    env = var.env
-    owner =  var.owner
-    app = var.app
-  }
-}
-
-
-locals {
-  create_bucket      = var.create_bucket
-  create_bucket_acl  = var.acl != null && var.acl != "null"
-  attach_policy      = var.attach_elb_log_delivery_policy || var.attach_lb_log_delivery_policy || var.attach_iam_policy || var.attach_cloudtrail_policy
-  cors_rules         = var.cors_rules
+  s3_bucket_tags = merge(
+    var.tags,
+    {
+      Customer-Code = var.s3_tags
+    }
+  )
 }
