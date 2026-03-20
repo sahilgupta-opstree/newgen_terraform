73f4da13ab82e2960d81bfebcf017879ad60f864
tarandeep singh <tarandeep.singh@opstree.com>
2026-03-20 18:10:35 +0530

    updated tgw_attachment_details

--- modules/terraform-aws-transit-gateway/variable.tf
+++ modules/terraform-aws-transit-gateway/variable.tf
@@ -4,6 +4,12 @@ variable "transit_gateway_name" {
   default     = "prod-tgw"
 }
 
+variable "tgw_arn" {
+  description = "The ARN of the existing Transit Gateway"
+  type        = string
+  default     = "arn:aws:ec2:us-east-1:069243561461:transit-gateway/tgw-12345678"  # Replace with your actual ARN
+}
+
 variable "tags" {
   type        = map(string)
   description = "Tags to apply to all resources"
