da336eebdb69f54c6007d9b3da1837c2871bfb3a
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-03-20 17:17:54 +0530

    Update local.tf

--- modules/terraform-aws-vpc-endpoints/local.tf
+++ modules/terraform-aws-vpc-endpoints/local.tf
@@ -1,8 +1,7 @@
 locals {
-  vpc_endpoint_tags = merge(
-    var.tags,
-    {
-      Customer-Code = var.vpc_endpoint_tags
-    }
-  )
+  # base_name is derived from the "Name" key in var.tags (set per-resource in tfvars)
+  base_name = lookup(var.tags, "Name", "")
+
+  # common_tags strips the "Name" key so each resource can set its own Name tag
+  common_tags = { for k, v in var.tags : k => v if k != "Name" }
 }
