35b38c74e2a97732fb142e7e9f5c3258400e6e4a
tarandeep singh <tarandeep.singh@opstree.com>
2026-03-20 21:07:55 +0530

    added aws-routing-transit-gateway

--- modules/terraform-aws-transit-gateway/output.tf
+++ modules/terraform-aws-transit-gateway/output.tf
@@ -1,11 +1,11 @@
-output "vpc_attachment_ids" {
-  value = { for k, v in aws_ec2_transit_gateway_vpc_attachment.tgw_attachment : k => v.id }
-}
-
-# output "aws_ec2_transit_gateway_vpc_attachment" {
-#   value       = aws_ec2_transit_gateway_vpc_attachment.tgw_attachment[each.key].id
-#   description = "ID of the aws_ec2_transit_gateway_vpc_attachment"
+# output "vpc_attachment_ids" {
+#   value = { for k, v in aws_ec2_transit_gateway_vpc_attachment.tgw_attachment : k => v.id }
 # }
+
+output "aws_ec2_transit_gateway_ids" {
+  value = { for k, v in aws_ec2_transit_gateway_vpc_attachment.tgw_attachment : k => v.transit_gateway_id }
+  description = "Map of VPC attachment names to their Transit Gateway IDs"
+}
 
 
 output "aws_ec2_transit_gateway_vpc_attachment_ids" {
