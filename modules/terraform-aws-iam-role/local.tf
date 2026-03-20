c45ab3057862f1269a5b0b8b5686fa5c093e63b5
deepakkushwaha78 <deepak.kushwaha@opstree.com>
2026-03-20 22:21:12 +0530

    Update local.tf

--- modules/terraform-aws-iam-role/local.tf
+++ modules/terraform-aws-iam-role/local.tf
@@ -39,10 +39,9 @@ locals {
     item.key => item
   }
 
-  iam_role_tags = merge(
-    var.tags,
-    {
-      Customer-Code = var.iam_role_tags
-    }
-  )
+  # base_name is derived from the "Name" key in var.tags (set per-resource in tfvars)
+  base_name = lookup(var.tags, "Name", "")
+
+  # common_tags strips the "Name" key so each resource can set its own Name tag
+  common_tags = { for k, v in var.tags : k => v if k != "Name" }
 }
